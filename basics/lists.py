2f3f3df3a705fd96854170f49d1eb709cacb457f
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-12 22:26:11 -0600

    word replacement practice

---
+++ basics/lists.py
@@ -0,0 +1,3 @@
+countries = ['USA', 'United Kingdom', 'Nigeria', 'Australia']
+
+print(countries)
---
+++ basics/word_replacement.py
@@ -0,0 +1,8 @@
+sentence = input('Enter your sentence: ')
+
+print('Your sentence is:', sentence)
+
+word_to_replace = input('Enter word to replace: ')
+replacement_word = input('Enter new word: ')
+
+print(sentence.replace(word_to_replace, replacement_word))
