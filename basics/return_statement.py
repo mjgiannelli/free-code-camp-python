e0848be64054b5c8e75d2ae96d57a0b7d301187d
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-12 23:54:29 -0600

    basic return statement practice

---
+++ basics/return_statement.py
@@ -0,0 +1,13 @@
+def my_function():
+    return 5 + 4
+
+print(my_function())
+
+def add_numbers(num1, num2):
+    return int(num1) + int(num2)
+
+num1 = (input('Enter first number: '))
+num2 = (input('Enter second number: '))
+
+print(add_numbers(num1, num2))
+
