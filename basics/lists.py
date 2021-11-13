4171fcd11ce1848bb27738afb8bc941bea06f8e9
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-12 22:41:12 -0600

    list basics

--- basics/lists.py
+++ basics/lists.py
@@ -1,3 +1,16 @@
-countries = ['USA', 'United Kingdom', 'Nigeria', 'Australia']
+# lists can have different types just like in javascript
+countries = ['USA', 'United Kingdom', 4, 'Nigeria', True, 'Australia']
+
+names = list(('Mark', 'Mat', 'Erica', 'Chris'))
+
+countries[0] = 'United States'
+names[0] = 'Dave'
 
 print(countries)
+print(len(countries))
+print(type(countries[2]))
+
+print(names)
+
+# List Methods
+
