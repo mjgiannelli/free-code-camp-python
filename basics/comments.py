4d42198c4db92c15827843053e5affe144512597
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-13 13:12:02 -0600

    learning comment basics

---
+++ basics/comments.py
@@ -0,0 +1,15 @@
+# print('Hello')
+print(1) #only this line will print because the print command above is commented out
+
+# can also use ''' (used for multiple lines)
+
+my_name = 'Mark'
+
+'''
+for letter in my_name:
+    print(letter)
+'''
+
+print(my_name)
+
+# same exact thing as javascript except different syntax
