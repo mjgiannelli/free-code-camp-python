d0599f344f206318e539ab29bd2bcf2cb7947b5c
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-14 16:17:19 -0600

    create first app for project and set up rendering root url

---
+++ django/myproject/myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #when first param is empty, we are creating a url path for the root page
+    # if want to build on the url you would input '/download' as an example for the first param
+    # 2nd param is a funtion called index you are importing used to render the html page. imported from the views.py file. Whatever index produces will be rendered to the user
+    # 3rd param name gives the url path a name - usually the same as the function that is being imported from the views file
+    path('', views.index, name='index')
+]
