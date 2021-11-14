c83f8cb64e8b555a5452d5df5233eabcd3de032d
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-13 22:17:22 -0600

    basic pip install instructions

---
+++ basics/pip.py
@@ -0,0 +1,6 @@
+# pip is basically the same thing as npm in javascript. it's an online source for different modules that users created that you can import into your project
+# site is called pypi.org
+# search for modules you would like to install and find the CL prompt needed to install
+    # example: pip install django-deep-serializer
+
+
