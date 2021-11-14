82524ed18b1723ca789c6af496b1610226b29a66
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-14 17:12:58 -0600

    set up views index function to pass a dictionary to render dynamic information to the static html file

--- django/myproject/myapp/views.py
+++ django/myproject/myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')
+    # create a dictionary of key/values to send to html static page
+    context = {
+        'name': 'Mark',
+        'age': 33,
+        'nationality': 'American'
+    }
+    return render(request, 'index.html', context)
