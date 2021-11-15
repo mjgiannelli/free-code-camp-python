9f0e9629abb9a017688496110dd061c4def5bdff
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-14 18:24:42 -0600

    set up a quick word counter application

--- django/myproject/myapp/urls.py
+++ django/myproject/myapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # if want to build on the url you would input '/download' as an example for the first param
     # 2nd param is a funtion called index you are importing used to render the html page. imported from the views.py file. Whatever index produces will be rendered to the user
     # 3rd param name gives the url path a name - usually the same as the function that is being imported from the views file
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('counter', views.counter, name='counter')
 ]
--- django/myproject/myapp/views.py
+++ django/myproject/myapp/views.py
@@ -11,3 +11,9 @@ def index(request):
         'nationality': 'American'
     }
     return render(request, 'index.html', context)
+
+def counter(request):
+    # to collect the words user is submitting in the form: get 'text' bc it's the same name attribute in the text area
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
