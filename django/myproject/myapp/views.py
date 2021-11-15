7d4e5fd1211b34162ddaa47c426105ed93da3d6b
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-14 21:09:53 -0600

    learning db model, view, template basics

--- django/myproject/myapp/views.py
+++ django/myproject/myapp/views.py
@@ -1,16 +1,37 @@
 from django.shortcuts import render
+# import your models (database api) to the views to pass to template
+from .models import Feature
 
 # Create your views here.
 
 
 def index(request):
     # create a dictionary of key/values to send to html static page
-    context = {
-        'name': 'Mark',
-        'age': 33,
-        'nationality': 'American'
-    }
-    return render(request, 'index.html', context)
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our service is very quick'
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Reliable'
+    feature2.details = 'Our service is very reliable'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Easy to Use'
+    feature3.details = 'Our service is very easy to use'
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Affordable'
+    feature4.details = 'Our service is very affordable'
+
+    features = [feature1, feature2, feature3, feature4]
+
+    return render(request, 'index.html', {
+        'features': features
+        })
 
 def counter(request):
     # to collect the words user is submitting in the form: get 'text' bc it's the same name attribute in the text area
