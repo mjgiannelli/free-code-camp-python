0faf3c5d412e6c6d94a812c92de278403cedd2cf
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-11-14 18:37:57 -0600

    practice between a get and a post method

--- django/myproject/myapp/views.py
+++ django/myproject/myapp/views.py
@@ -14,6 +14,6 @@ def index(request):
 
 def counter(request):
     # to collect the words user is submitting in the form: get 'text' bc it's the same name attribute in the text area
-    text = request.GET['text']
+    text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
